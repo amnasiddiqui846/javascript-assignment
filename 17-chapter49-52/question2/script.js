3ef325ddced4d7b793ee674e18a967a2e974dc06
Amna Siddiqui <amnasiddiqui846@gmail.com>
2026-05-11 01:23:33 +0500

    up

--- 17-chapter49-52/question2/script.js
+++ 17-chapter49-52/question2/script.js
@@ -1,16 +1,4 @@
-
-function toggleText() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("moreText");
-    var btn = document.getElementById("btn");
-
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        moreText.style.display = "none";
-        btn.innerHTML = "Read More";
-    } else {
-        dots.style.display = "none";
-        moreText.style.display = "inline";
-        btn.innerHTML = "Read Less";
+ function showMore() {
+      document.getElementById("more").style.display = "inline";
+      document.getElementById("btn").style.display = "none";
     }
-}
